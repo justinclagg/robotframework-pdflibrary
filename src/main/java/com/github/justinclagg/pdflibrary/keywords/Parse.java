e94f029c2ab3acd85ebad04f1d53228a16ace5dc
Justin Clagg <justinclagg@gmail.com>
2017-09-16 15:55:13 -0400

    added getNumberOfPdfPages keyword, added ignoreCase parameter to PdfShouldContain keyword

--- src/main/java/com/github/justinclagg/pdflibrary/keywords/Parse.java
+++ src/main/java/com/github/justinclagg/pdflibrary/keywords/Parse.java
@@ -8,6 +8,7 @@ import java.util.Map;
 import org.apache.commons.lang.StringUtils;
 import org.robotframework.javalib.annotation.ArgumentNames;
 import org.robotframework.javalib.annotation.RobotKeyword;
+import org.robotframework.javalib.annotation.RobotKeywordOverload;
 import org.robotframework.javalib.annotation.RobotKeywords;
 
 import com.itextpdf.text.pdf.PdfReader;
@@ -18,36 +19,47 @@ public class Parse {
 
 	private PdfReader reader;
 	private Map<Integer, String> data;
-
-	@RobotKeyword("Parse a PDF")
-	@ArgumentNames({ "filename" })
-	public void parsePdf(String filename) throws IOException {
-		reader = new PdfReader(filename);
-
+	private int numberOfPages;
+	
+	@RobotKeyword("Parses a PDF and stores its text.")
+	@ArgumentNames({ "file" })
+	public void parsePdf(String file) throws IOException {
+		reader = new PdfReader(file);
 		data = new HashMap<Integer, String>();
-		int numberOfPages = reader.getNumberOfPages();
+		numberOfPages = getNumberOfPdfPages();
+		
 		for (int page = 1; page <= numberOfPages; page++) {
 			System.out.println("Reading page " + page);
 			String textFromPage = PdfTextExtractor.getTextFromPage(reader, page);
 			data.put(page, textFromPage);
 		}
 	}
-
-	@RobotKeyword("Verify that the given value exists in the parsed PDF.")
-	@ArgumentNames({ "expectedValue" })
-	public void pdfShouldContain(String expectedValue) {
+	
+	@RobotKeyword("Returns the number of pages in the parsed PDF.")
+	public int getNumberOfPdfPages() {
+		return reader.getNumberOfPages();
+	}
+	
+	@RobotKeyword("Verifies that the given text exists in the parsed PDF.")
+	@ArgumentNames({ "expectedText", "ignoreCase=false" })
+	public void pdfShouldContain(String expectedText, boolean ignoreCase) {
 		Collection<String> values = data.values();
-		collectionShouldContain(expectedValue, values, true);
+		collectionShouldContain(expectedText, values, ignoreCase);
+	}
+	
+	@RobotKeywordOverload
+	public void pdfShouldContain(String expectedText) {
+		pdfShouldContain(expectedText, false);
 	}
 
-	private void collectionShouldContain(String expectedValue, Collection<String> values, boolean ignoreCase) {
+	private void collectionShouldContain(String expectedText, Collection<String> values, boolean ignoreCase) {
 		for (String content : values) {
-			if (ignoreCase ? StringUtils.containsIgnoreCase(content, expectedValue)
-					: StringUtils.contains(content, expectedValue)) {
+			if (ignoreCase ? StringUtils.containsIgnoreCase(content, expectedText)
+					: StringUtils.contains(content, expectedText)) {
 				return;
 			}
 		}
-		throw new RuntimeException("could not find " + expectedValue + " in " + data);
+		throw new RuntimeException("could not find " + expectedText + " in " + data);
 	}
 
 }
