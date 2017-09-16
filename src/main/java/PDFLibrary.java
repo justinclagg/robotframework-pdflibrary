75b1da6d4c35b888bdf123b4c38f596e09c1fc03
Justin Clagg <justinclagg@gmail.com>
2017-09-16 14:46:00 -0400

    adjusted keyword path, initial keywords are functional

--- src/main/java/PDFLibrary.java
+++ src/main/java/PDFLibrary.java
@@ -1,7 +1,19 @@
-public class PDFLibrary extends com.github.justinclagg.pdflibrary.PDFLibrary {
+import org.robotframework.javalib.library.AnnotationLibrary;
+
+public class PDFLibrary extends AnnotationLibrary {
+
+	public static final String ROBOT_LIBRARY_SCOPE = "GLOBAL";
 
 	public PDFLibrary() {
-		super();
+		super("com/github/justinclagg/pdflibrary/keywords/**");
 	}
-	
+
+	@Override
+	public String getKeywordDocumentation(String keywordName) {
+		if (keywordName.equals("__intro__")) {
+			return "PDFLibrary is a Robot Framework library for testing PDFs.";
+		}
+		return super.getKeywordDocumentation(keywordName);
+	}
+
 }
